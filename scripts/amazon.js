7bfcbb14324e54cf1379476ed165854b6fcbf343
callecandelier-maker <callecandelier@gmail.com>
2026-01-03 13:46:27 +0100

    Make add to cart button intercative

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -54,7 +54,9 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button 
+          button-primary js-add-to-cart"
+          data-product-id="${product.id}"> 
             Add to Cart
           </button>
         </div>
@@ -64,4 +66,46 @@ products.forEach((product) => {
 // using the DOM to generate all the html
 document.querySelector('.js-products-grid')
     .innerHTML = productsHTML;
+
+
+// Select ALL elements on the page with the class "ja-add-to-cart"
+// querySelectorAll returns a list (NodeList) of matching elements
+document.querySelectorAll('.js-add-to-cart')
+// forEach loops through the list — one button at a time
+    .forEach((button) => {
+    // For each button, we add a "click" event listener
+      button.addEventListener('click', () => {
+        // add the product and the quantity to the cart
+        // data atribute it allows to attach any information to an element
+        // Get the attribute from the button and displays it
+        // productId converts from kebab-case to Camel-case 
+        // from the data attribbute in the DOM button
+        const productId = button.dataset.productId;
+        // a global variable that we use to store the value that is item
+        let matchingItem; 
+
+        cart.forEach((item) => {
+            // Checking if we already have this product in the cart
+            if(productId === item.productId){
+                matchingItem = item;
+            }   
+        })
+
+        if(matchingItem){
+            // if we got a matchingItem increase its quantity by 1
+            matchingItem.quantity += 1;
+        } else{
+            //If not:
+            // place the chossen object in the list when we push a button
+            cart.push({
+            productId: productId,
+            quantity: 1
+        });
+        }
+
+        console.log(cart);
+      });  
+    });
+
+
 
