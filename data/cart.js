0f70d9516971d64d8f05f86570371e85797fe4c3
callecandelier-maker <callecandelier@gmail.com>
2026-01-05 17:40:29 +0100

    Save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,9 @@
 // Adding products and quantity
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+
+if(!cart){
+    cart = [{
     // normalizing the data
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2
@@ -7,6 +11,12 @@ export let cart = [{
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity: 1
 }];
+
+}
+
+function saveToStorage(){
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 // -----------------------------------------------------------------
 // Adds a product to the cart.
@@ -41,6 +51,8 @@ export function addToCart(productId){
         quantity: 1
     });
     }
+
+    saveToStorage();
 }
 
 
@@ -61,5 +73,7 @@ export function removeFromCart(productId){
     });
 
     cart = newCart;
+
+    saveToStorage();
 
 }
