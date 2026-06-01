c8703d43215e5f514aaf055f4331080efe61d3fb
callecandelier-maker <callecandelier@gmail.com>
2026-06-01 14:02:15 +0200

    Save data and generate html for delivery options

--- data/cart.js
+++ data/cart.js
@@ -6,16 +6,17 @@ if (!cart){
     // normalizing the data
     
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2
-    
+    quantity: 2,
+    deliveryOptionId: '1'
 }, {
     
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1
+    quantity: 1,
+    deliveryOptionId: '2'
     
 }]; 
 }
-
+//console.log('Cart in cart.js:', cart);//
 function saveToStorage() {
     localStorage.setItem('cart', JSON.stringify(cart));
 }
@@ -50,7 +51,8 @@ export function addToCart(productId){
         // Otherwise: add the product to the cart as a new entry
         cart.push({
         productId: productId,
-        quantity: 1
+        quantity: 1,
+        deliveryOptionId: '1'
     });
     }
 
