a25e50e41a3932d9cccc59d9451bcabb434c1000
callecandelier-maker <callecandelier@gmail.com>
2026-06-04 11:12:04 +0200

    Convert products into classes

--- data/products.js
+++ data/products.js
@@ -1,4 +1,6 @@
 
+import {formatCurrency} from '../scripts/utils/money.js';
+
 export function getProduct(productId) {
    let matchingProduct;
 
@@ -12,6 +14,32 @@ export function getProduct(productId) {
       });
 
       return matchingProduct;
+}
+
+class product {
+  id; 
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStarsUrl() {
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  getPrice() {
+    return `$${formatCurrency(this.priceCents)}`;
+  }
+ 
+  
 }
 
 
@@ -674,4 +702,6 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails) => {
+  return new product(productDetails);
+});
