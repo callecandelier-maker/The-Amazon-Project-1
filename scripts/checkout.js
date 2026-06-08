267ceb65f485187d4f4157926110329f65402946
callecandelier-maker <callecandelier@gmail.com>
2026-06-08 10:06:50 +0200

    Convert loadProducts from XHR to fetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,18 +1,13 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
-import { loadProducts, products } from '../data/products.js';
+import { loadProducts, products, loadProductsFetch } from '../data/products.js';
 import { loadCart } from '../data/cart.js';
 //import '../data/cart-class.js';
 //import '../data/car.js';
 //import '../data/backend-practice.js';
 
 Promise.all([
-        new Promise((resolve) => {
-        loadProducts(() => {
-            resolve('value1');
-        });
-
-    }),
+       loadProductsFetch(),
         new Promise((resolve) => {
         loadCart(() => {
             resolve();
--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 import { loadFromStorage, cart } from "../../data/cart.js";
-import { loadProducts } from "../../data/products.js";
+import { loadProducts, loadProductsFetch } from "../../data/products.js";
 
 
 describe('test suite: renderOrderSummary', () => {
@@ -9,7 +9,7 @@ describe('test suite: renderOrderSummary', () => {
 
 
     beforeAll((done) => {
-        loadProducts(() => {
+        loadProductsFetch().then(() => {
             done();
         });
     });
